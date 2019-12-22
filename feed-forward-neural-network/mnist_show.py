f71738a21054de5d34693f28b36551491741058f
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-12-22 14:12:34 -0800

    add functionality to show image

--- feed-forward-neural-network/mnist_show.py
+++ feed-forward-neural-network/mnist_show.py
@@ -2,6 +2,11 @@ import sys, os
 sys.path.append(os.pardir)
 import numpy as np
 from dataset.mnist import load_mnist
+from PIL import Image # pip install Pillow
+
+def img_show(img):
+  pil_img = Image.fromarray(np.uint8(img))
+  pil_img.show()
 
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 
@@ -9,3 +14,12 @@ print(x_train.shape)
 print(t_train.shape)
 print(x_test.shape)
 print(t_test.shape)
+
+img = x_train[0]
+label = t_train[0]
+print(img)
+print(label)
+img = img.reshape(28, 28)
+print(img.shape)
+
+img_show(img)
