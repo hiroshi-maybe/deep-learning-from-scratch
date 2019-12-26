3daff8ccf7804284873b6cd10c750f7ceedddc01
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-12-25 16:45:17 -0800

    add accuracy evaluation per epoch

--- training/train_neuralnet_mnist.py
+++ training/train_neuralnet_mnist.py
@@ -7,12 +7,16 @@ from two_layer_net import TwoLayerNet
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
 train_loss_list = []
+train_acc_list = []
+test_acc_list = []
 
 # Hyper parameters
 iters_num = 10000
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
+
+iter_per_epoch = max(train_size/batch_size, 1)
 
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
@@ -23,6 +27,7 @@ for i in range(iters_num):
   
   # gradient computation
   grad = network.numerical_gradient(x_batch, t_batch)
+  # grad = network.gradient(x_batch, t_batch)
   
   # update model parameters
   for key in ('W1', 'b1', 'W2', 'b2'):
@@ -31,4 +36,11 @@ for i in range(iters_num):
   # track training
   loss = network.loss(x_batch, t_batch)
   train_loss_list.append(loss)
-
+  
+  # calc accuracy per epoch
+  if i % iter_per_epoch == 0:
+    train_acc = network.accuracy(x_train, t_train)
+    test_acc = network.accuracy(x_test, t_test)
+    train_acc_list.append(train_acc)
+    test_acc_list.append(test_acc)
+    print(f"train acc, test acc | {str(train_acc)}, {str(test_acc)}")
