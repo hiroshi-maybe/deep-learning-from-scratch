e6a3207b2dc973270bc6949e05e170779df4b411
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-01-04 17:59:26 -0800

    add softmax with cross entropy loss

--- backward-propagation/layers.py
+++ backward-propagation/layers.py
@@ -1,3 +1,7 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from common.functions import *
 
 class MultiLayer:
   def __init__(self):
@@ -79,6 +83,25 @@ class Affine:
     dw = np.dot(self.X.T, dout)
     # ∂L/∂B = ∑ { ∂L/∂Y_{i,j} : i }
     db = np.sum(dout, axis=0)
+    
+    return dx
+
+class SoftmaxWithLoss:
+  def __init__(self):
+    self.loss = None
+    self.y = None # ouput of softmax
+    self.t = None # training data in one-hot format
+  
+  def forward(self, x, t):
+    self.t = t
+    self.y = softmax(x)
+    self.loss = cross_entropy_error(self.y, self.t)
+    
+    return self.loss
+  
+  def backward(self, dout=1):
+    batch_size = self.t.shape[0]
+    dx = (self.y - self.t) / batch_size
     
     return dx
 
