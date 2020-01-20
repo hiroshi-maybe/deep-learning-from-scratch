741715c4176c5a12b3e3538f10def8616ca0e5c9
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-01-20 15:34:49 -0800

    add convolution layer

--- common/layers.py
+++ common/layers.py
@@ -2,6 +2,7 @@ import sys, os
 sys.path.append(os.pardir)
 import numpy as np
 from common.functions import *
+from common.util import im2col, col2im
 
 class MultiLayer:
   def __init__(self):
@@ -213,6 +214,44 @@ class BatchNormalization:
     self.dgamma = dgamma
     self.dbeta = dbeta
     
+    return dx
+
+class Convolution:
+  def __init__(self, W, b, stride=1, pad=0):
+    self.W=W
+    self.b=b
+    self.stride=stride
+    self.pad=pad
+
+  def forward(self, x):
+    FN,C,FH,FW=self.W.shape
+    N,C,H,W=x.shape
+    out_h = int((H+2*self.pad-FH)/self.stride) + 1
+    out_w = int((W+2*self.pad-FW)/self.stride) + 1
+
+    col=im2col(x, FH, FW, self.stride, self.pad)
+    col_W = self.W.reshape(FN,-1).T
+
+    out = np.dot(col, col_W) + self.b
+    out = out.reshape(N, out_h, out_w, -1).transpose(0,3,1,2)
+
+    self.x = x
+    self.col = col
+    self.col_W = col_W
+
+    return out
+
+  def backward(self, dout):
+    FN, C, FH, FW = self.W.shape
+    dout = dout.transpose(0,2,3,1).reshape(-1, FN)
+
+    self.db = np.sum(dout, axis=0)
+    self.dW = np.dot(self.col.T, dout)
+    self.dW = self.dW.transpose(1,0).reshape(FN,C,FH,FW)
+
+    dcol = np.dot(dout, self.col_W.T)
+    dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
+
     return dx
 
 def nearlyEqual(x, y):
--- common/util.py
+++ common/util.py
@@ -24,3 +24,65 @@ def shuffle_dataset(x, t):
   t = t[permutation]
 
   return x,t
+
+def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
+  """Reshape to 2d matrix to forward in convolutional NN
+
+  Parameters
+  ----------
+  input_data : (batch size, channel, height, width)
+  filter_h : height of the filter
+  filter_w : width of the filter
+  stride : stride size
+  pad : padding size
+
+  Returns
+  -------
+  res : 2d array
+  """
+  N,C,H,W = input_data.shape
+  out_h = (H+2*pad-filter_h)//stride+1
+  out_w = (W+2*pad-filter_w)//stride+1
+
+  # pad input_data in vertical and horizontal axis
+  img = np.pad(input_data, [(0,0), (0,0), (pad,pad), (pad,pad)], 'constant')
+  res = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
+
+  for y in range(filter_h):    
+    y_max = y + stride*out_h
+    for x in range(filter_w):
+        x_max = x + stride*out_w
+        res[:,:,y,x,:,:] = img[:,:,y:y_max:stride, x:x_max:stride]    
+  res = res.transpose(0,4,5,1,2,3).reshape(N*out_w*out_w, -1)
+  return res
+
+def col2im(col, input_syape, filter_h, filter_w, stride=1, pad=0):
+  """Reshape to 4d matrix to backward in convolutional NN
+
+  Parameters
+  ----------
+  col : 2d matrix to reshape
+  input_shape: shape of input
+  filter_h : height of the filter
+  filter_w : width of the filter
+  stride: stride size
+  pad : paddingsize
+
+  Returns
+  -------
+  img
+  """
+
+  N,C,H,W = input_shape
+  out_h = (H+2*pad-filter_h)//stride+1
+  out_w = (W+2*pad-filter_w)//stride+1
+  col = col.reshape(N, out_h, out_w, C, filter_h, filter_w).transpose(0,3,4,5,1,2)
+
+  img = np.zeros((N,C,H+2*pad_stride-1, W+2*pad+stride-1))
+  for y in range(filter_h):
+    y_max = y+stride*out_h
+    for x in range(filter_w):
+      x_max = x+stride*out_w
+      img[:, :, y:y_max:stride, x:x_max:stride] += col[:, :, y, x, :, :]
+  
+  return img[:, :, pad:H+pad, pad:W+pad]
