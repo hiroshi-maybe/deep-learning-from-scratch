652c6a2f8f404b11e65d43373def386e6ba357bb
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-01-20 18:50:15 -0800

    add visualized filter models

--- convolutional-nn/simple_convnet.py
+++ convolutional-nn/simple_convnet.py
@@ -99,3 +99,13 @@ class SimpleConvNet:
       params[key] = val
     with open(file_name, 'wb') as f:
       pickle.dump(params, f)
+
+  def load_params(self, file_name="params.pkl"):
+    with open(file_name, 'rb') as f:
+      params = pickle.load(f)
+    for key, val in params.items():
+      self.params[key] = val
+    
+    for i, key in enumerate(['Conv1', 'Affine1', 'Affine2']):
+      self.layers[key].W = self.params['W' + str(i+1)]
+      self.layers[key].b = self.params['b' + str(i+1)]
